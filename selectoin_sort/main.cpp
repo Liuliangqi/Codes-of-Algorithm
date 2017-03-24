e0bf8f58a79428bb1debd5d93a7f4ef27e0a61e7
Liuliangqi <lliu025@fiu.edu>
2017-03-24 00:00:55 -0700

    Create main.cpp

---
+++ selectoin_sort/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "SortTestHelper.h"
+
+using namespace std;
+
+//regard current num as minIndex, compare it with all following nums
+//let minIndex point to the smallest num in the remaining nums, and swap it with current num
+template<typename T>
+void selectionSort(T arr[], int n){
+    for(int i = 0; i < n; i++){
+        int minIndex = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+    
+    int main(){
+        int n = 10000;
+        int* a = SortTest::generateRandomArray(n, 0, 100);
+        SortTest::testSort("Selection Sort", selectionSort, a, n);
+        
+        delete[] a;
+        return 0;
+    }
+    
+}
