6cc8a8a07f8e2d85628d649d2fe3d338ee455abf
Liangqi Liu <lliu025@fiu.edu>
2017-03-24 02:06:34 -0700

    Add files via upload

--- selectoin_sort/SortTestHelper.h
+++ selectoin_sort/SortTestHelper.h
@@ -1,71 +1,67 @@
-//created by Liangqi
+//
+// Created by 刘亮奇 on 2017/3/13.
+//
+
 #ifndef ALGORITHM_SORTTESTHELPER_H
 #define ALGORITHM_SORTTESTHELPER_H
+
 #include <cassert>
 #include <ctime>
 #include <iostream>
 using namespace std;
-
-//create a new namespace
 namespace SortTest{
     int* generateRandomArray(int n, int rangeL, int rangeR){
-        //if rangeL > rangeR, stop the code and print some error information
         assert(rangeL <= rangeR);
-        int* arr = new int[n]l
-        //initial seed
+        int* arr = new int[n];
         srand(time(NULL));
         for(int i = 0; i < n; i++){
             arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
         }
+
         return arr;
     }
-    
-    //print array
-    template<typename T>
+
+    template <typename T>
     void printArray(T* arr, int n){
         for(int i = 0; i < n; i++){
             cout << arr[i] << " ";
         }
         cout << endl;
     }
-    
-    //verify sort result
-    template<typename T>
+
+
+    template <typename T>
     bool isSorted(T arr[], int n){
-        for(int i = 0; i < n-1; i++){
-            if(arr[i] > arr[i + 1]){
+        for(int i = 0; i < n - 1; i++){
+            if(arr[i] > arr[i+1]){
                 return false;
             }
         }
         return true;
     }
-    //test sort time and result
-    template<typename T>
-    void testSort(string name, void(*sort)(T[], int), T arr[], int n){
+
+    template <typename T>
+    void testSort(string sortName, void(*sort)(T[], int), T arr[], int n){
         clock_t startTime = clock();
         sort(arr, n);
         clock_t endTime = clock();
-        
+
         assert(isSorted(arr, n));
         cout << sortName << " : " << double(endTime - startTime) / CLOCKS_PER_SEC << " s;" << endl;
         return;
     }
-    
-    
-    //copy int array
+
     int* copyIntArray(int* arr, int n){
         int* a = new int[n];
         copy(arr, arr+n, a);
         return a;
     }
-    
-    //generate an array that less random
+
     int* generateLessRandomArray(int n, int randomTime){
         int* arr = new int[n];
         for(int i = 0; i < n; i++){
             arr[i] = i;
         }
-        
         srand(time(NULL));
         for(int i = 0; i < randomTime; i++){
             int posX = rand() % n;
@@ -74,4 +70,7 @@ namespace SortTest{
         }
         return arr;
     }
-}
+};
+
+
+#endif //ALGORITHM_SORTTESTHELPER_H
