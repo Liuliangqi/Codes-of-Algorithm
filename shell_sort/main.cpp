80778faa109ad40e95c518103476c68d50d3f593
Liangqi Liu <lliu025@fiu.edu>
2017-03-24 15:08:18 -0700

    Update main.cpp

--- shell_sort/main.cpp
+++ shell_sort/main.cpp
@@ -5,4 +5,22 @@
 using namespace std;
 
 template<typename T>
-void shellSort(){}
+void shellSort(T arr[], int n){
+    for(int gap = n / 2; gap > 0; gap /= 2){
+        for(int i = gap; i < n; i++){
+            for(int j = i - gap; j >= 0 && arr[j] > arr[j + gap]; j--){
+                swap(arr[j], arr[j+gap]);
+            }
+        }
+    }
+}
+
+
+int main(){
+    int n = 10000;
+    int* a = SortTest::generateRandomArray(n, 0, 100);
+    SortTest::testSort("Shell Sort", shellSort, a, n);
+  
+    delete[] a;
+    return 0;
+}
